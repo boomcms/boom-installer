21a8d053a12cc4b39e679c6124493128fc52663c
Rob Taylor <rob@uxblondon.com>
2015-06-08 11:23:33 +0100

    Updated installer service provider to run install commands on boot

--- src/BoomCMS/Installer/ServiceProvider.php
+++ src/BoomCMS/Installer/ServiceProvider.php
@@ -2,27 +2,35 @@
 
 namespace BoomCMS\Installer;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
     /**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-    public function boot()
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(Request $request)
     {
         // On HTTP Post request check for install data in $_POST and complete install if required.
 
-        // $this->dispatch('BoomCMS\Core\Commands\CreatePerson', [$config['user_name'], $config['user_email'], []]);
-        // $installer->install($_POST);
+        $installer = new Installer();
+
+        if ( ! $installer->isInstalled()) {
+            $this->dispatch('Illuminate\Database\Console\Migrations\InstallCommand');
+            $this->dispatch('Illuminate\Database\Console\Migrations\MigrateCommand');
+            $this->dispatch('BoomCMS\Core\Commands\CreatePerson', [$request->input('user_name'), $request->input('user_email'), []]);
+            $this->dispatch('BoomCMS\Core\Commands\CreatePage');
+            $installer->markInstalled();
+        }
     }
 
     /**
-	 *
-	 * @return void
-	 */
+     *
+     * @return void
+     */
     public function register()
     {
         $installer = new Installer();
