cc785bbd5dd0b36066a81fef3c485a3f10b1f980
Rob Taylor <rob@uxblondon.com>
2015-07-13 08:54:32 +0100

    Update reference to settings facade

--- src/BoomCMS/Installer/Installer.php
+++ src/BoomCMS/Installer/Installer.php
@@ -2,7 +2,7 @@
 
 namespace BoomCMS\Installer;
 
-use BoomCMS\Core\Facades\Settings;
+use BoomCMS\Support\Facades\Settings;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
