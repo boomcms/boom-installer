63b5ca6988694d5812e9b26e99ce8054934a90b6
Rob Taylor <rob@uxblondon.com>
2015-08-13 15:13:50 +0100

    Fixed command namespaces in InstallerServiceProvider

--- src/BoomCMS/ServiceProviders/InstallerServiceProvider.php
+++ src/BoomCMS/ServiceProviders/InstallerServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace BoomCMS\ServiceProviders;
 
-use BoomCMS\Commands\CreatePerson as CreatePersonCommand;
+use BoomCMS\Commands;
 use BoomCMS\Core\Auth;
 use BoomCMS\Installer;
 use Illuminate\Foundation\Bus\DispatchesCommands;
@@ -30,7 +30,7 @@ class InstallerServiceProvider extends BaseServiceProvider
             $this->app['migrator']->run(base_path('/vendor/boomcms/boom-core/src/database/migrations'));
             $installer->saveSiteDetails($request->input('site_name'), $request->input('site_email'));
 
-            $person = $this->dispatch(new CreatePersonCommand([
+            $person = $this->dispatch(new Commands\CreatePerson([
                     'name'      => $request->input('user_name'),
                     'email'     => $request->input('user_email'),
                     'superuser' => true,
@@ -39,8 +39,8 @@ class InstallerServiceProvider extends BaseServiceProvider
 
             $auth->login($person);
 
-            $page = $this->dispatch(new \BoomCMS\Core\Commands\CreatePage($this->app['boomcms.page.provider'], $auth));
-            $this->dispatch(new \BoomCMS\Core\Commands\CreatePagePrimaryUri($this->app['boomcms.page.provider'], $page, '', '/'));
+            $page = $this->dispatch(new Commands\CreatePage($this->app['boomcms.page.provider'], $auth));
+            $this->dispatch(new Commands\CreatePagePrimaryUri($this->app['boomcms.page.provider'], $page, '', '/'));
             $installer->markInstalled();
 
             header('Location: /');
